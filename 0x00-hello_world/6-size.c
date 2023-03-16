5dd660488aac33e405f22a84806a1309002ceeac
Kevin Warui <waruimacharia@gmail.com>
2023-03-16 04:59:44 -0700

    Add "\n" to end of printf statements

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	printf("The size of a char: %d byte(s)", sizeof(char));
-	printf("The size of an int: %d byte(s)", sizeof(int));
-	printf("The size of a long int: %d byte(s)", sizeof(long int));
-	printf("The size of a long long int: %d byte(s)", sizeof(long long int));
-	printf("The size of a float: %d byte(s)", sizeof(float));
+	printf("The size of a char: %d byte(s)\n", sizeof(char));
+	printf("The size of an int: %d byte(s)\n", sizeof(int));
+	printf("The size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("The size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("The size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
