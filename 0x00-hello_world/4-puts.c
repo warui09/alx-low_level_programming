14f4bd597a2a9aa7b00d0d7aeb587b55553656be
Kevin Warui <waruimacharia@gmail.com>
2023-03-16 04:12:31 -0700

    Edit comment for the main function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- * main function prints characters to standard output
+ * main - print characters to standard output
+ * Description: print the given string to the standard output
+ * Return: return 0 on success
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
