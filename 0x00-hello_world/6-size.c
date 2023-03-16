1145cea60a66897142262659b15e267d6ca73588
Kevin Warui <waruimacharia@gmail.com>
2023-03-16 05:15:26 -0700

    Edit main function description

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,13 @@
 /**
  * main - print size of various types 
  * Description: print size of various types on the computer that this code will run on
- * Return 0 on success
+ * Return: 0 on success
  */
 
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of an int: %d byte(s)\n", sizeof(int))
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
