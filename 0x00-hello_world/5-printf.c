be9f867023f9c6d6cb78d2d939ce9e66b329563d
Kevin Warui <waruimacharia@gmail.com>
2023-03-16 04:19:10 -0700

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print characters to standard output
+ * Description: use the printf function to print characters to standard output
+ * Return: return 0 on success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
