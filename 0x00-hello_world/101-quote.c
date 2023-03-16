f2fee17be0bb316df2c4df15cfe9bab52476460b
Kevin Warui <waruimacharia@gmail.com>
2023-03-16 08:17:33 -0700

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print characters to standard error
+ * Description: print a string of characters
+ * to the standard error
+ * Return: Return 1 on error exit
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
