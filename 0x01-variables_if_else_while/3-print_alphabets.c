469df62a3c5f0facab14dfc096c6fd29513d0473
Kevin Warui <waruimacharia@gmail.com>
2023-03-17 01:54:14 -0700

    Add 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ * Description: Print lower case and upper case
+ * characters
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char i;
+	char j;
+	for(i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for(j = 'A'; j <= 'Z'; j++)
+		putchar(j);
+	putchar('\n');
+
+	return (0);
+}
