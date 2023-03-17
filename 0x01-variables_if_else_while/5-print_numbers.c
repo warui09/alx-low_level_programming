7647246fed1c9d9633aa613a34914cdacaff8d8f
Kevin Warui <waruimacharia@gmail.com>
2023-03-17 04:52:09 -0700

    Add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: print base 10 numbers starting at 0
+ *
+ *Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
