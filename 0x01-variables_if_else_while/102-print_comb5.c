576caf74bf078bc63de614590f40428224616afd
Kevin Warui <waruimacharia@gmail.com>
2023-03-17 11:57:19 -0700

    Edit 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,11 +17,11 @@ int main(void)
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = a + 1; b <= '9'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			for (c = b + 1; c <= '9'; c++)
+			for (c = '0'; c <= '9'; c++)
 			{
-				for (d = c + 1; d <= '9'; d++)
+				for (d = 1; d <= '9'; d++)
 				{
 					putchar(a);
 					putchar(b);
