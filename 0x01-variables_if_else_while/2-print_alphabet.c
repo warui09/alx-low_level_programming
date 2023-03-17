b095c530dcaf3eae5093a81d3bd170fdd797adcc
Kevin Warui <waruimacharia@gmail.com>
2023-03-17 01:44:18 -0700

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ * Description: Print out alphabets in lower case
+ *
+ * Return: return 0 on success
+ */
+
+int main(void)
+{
+	char i;
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
