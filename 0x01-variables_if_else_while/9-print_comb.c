1734670b3363607071db0fb18c31ec2ebb8b506b
Kevin Warui <waruimacharia@gmail.com>
2023-03-17 05:55:23 -0700

    Edit 9-print_comb.c to use 4 putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,14 +14,11 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
+		putchar(i);
 		if (i != '9')
 		{
-			putchar(i);
-			putchar(', ');
-		}
-		else
-		{
-			putchar(i);
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
