8b8940e057609c1790eb131e745c1cccae6cf957
Kevin Warui <waruimacharia@gmail.com>
2023-03-17 02:11:16 -0700

    Add 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Print lower case letters except q and e
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i !== 'q')
+			putchar(i);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
