46a4353a55ea1212b5dd70d89c69a688d0d13f6c
Kevin Warui <waruimacharia@gmail.com>
2023-03-21 08:49:34 -0700

    Refactor "3-islower.c" to include "3-main.c"

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,25 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- *main - entry point
- *Description: check for lowercase
+ *_islower - check if lowercase
+ *Description: check if character is lowercase
  *
  *Return: 1 if lowercase, 0 otherwise
  */
 
-int _islower(int c);
-
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	return (0);
-}
-
 int _islower(int c)
 {
-	if (97 <= c || c <= 122)
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
