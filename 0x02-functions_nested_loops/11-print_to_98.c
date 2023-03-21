9ecb111ed948d1fd3056dc01a62b5c45579c8972
Kevin Warui <waruimacharia@gmail.com>
2023-03-21 14:38:02 -0700

    Edit for loop to count down to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (i = n; i < 99; i++)
+		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
@@ -24,10 +24,10 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (i = 98; i <= n; i++)
+		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
-			if (i != n)
+			if (i != 98)
 			{
 				printf(",");
 				printf(" ");
