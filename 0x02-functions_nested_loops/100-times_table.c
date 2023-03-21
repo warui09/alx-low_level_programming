8b30780a3bc40c7386a9d99704b504318df4c8bf
Kevin Warui <waruimacharia@gmail.com>
2023-03-21 15:39:49 -0700

    Add 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,5 +7,40 @@
 
 void print_times_table(int n)
 {
-	printf(n);
+	int i;
+	int j;
+	int k;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+				if (k >= 100)
+				{
+					printf("%d", k);
+					if (j != n)
+						printf(", ");
+				}
+				else if (k >= 10 && k < 100)
+				{
+					printf(" ");
+					printf("%d", k);
+					if (j != n)
+						printf(", ");
+				}
+				else
+				{
+					printf("  ");
+					printf("%d", k);
+					if (j != n)
+						printf(", ");
+				}
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
 }
