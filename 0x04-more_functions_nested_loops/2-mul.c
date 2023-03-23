8d8b06134b3ae6840d6f98ea677331a2c27c81ad
Kevin Warui <waruimacharia@gmail.com>
2023-03-23 00:24:07 -0700

    Add 2-main.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - return product of two ints
+ *@a: one int to be multiplied
+ *@b: one int to be multiplied
+ *Return: int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
