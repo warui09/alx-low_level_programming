b011652c7ba5b6380688f0bc8c971d985b96f953
Kevin Warui <waruimacharia@gmail.com>
2023-03-23 07:13:58 -0700

    Add 10-print_triangles.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *print_triangle - print triangle
+ *@size: height of triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k, n;
+
+	n = size - 1;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = n; j > i; j--)
+			{
+				_putchar(' ');
+			}
+			
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
