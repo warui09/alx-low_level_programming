75ac323dbe1d11389521a20f08ea61b3c75ae080
Kevin Warui <waruimacharia@gmail.com>
2023-03-27 11:02:24 -0700

    Edit 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 #define PASSWORD_LENGTH 8
 
@@ -8,22 +9,21 @@
  *main - genrate valid passwords for 101-crackme
  */
 
-int main(void)
-{
-	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[]<>?";
-	const int charset_size = sizeof(charset) - 1;
+int main() {
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const int charset_size = sizeof(charset) - 1;
     char password[PASSWORD_LENGTH + 1];
     int i;
 
-    srand(time(NULL));
+    srand(time(NULL) ^ getpid());
 
-    for (i = 0; i < PASSWORD_LENGTH; i++)
-    {
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
         password[i] = charset[rand() % charset_size];
     }
     password[PASSWORD_LENGTH] = '\0';
 
-    printf("Generated password: %s\n", password);
+    printf("%s", password);
 
-    return (0);
+    return 0;
 }
+
