227a0113afba22d4283a43c936b87d4796aafc2d
Kevin Warui <waruimacharia@gmail.com>
2023-03-27 04:41:22 -0700

    Add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string to print
+ */
+
+void print_rev(char *s)
+
+{
+	int i, n;
+
+	n = 0;
+	while (s[n] != '\0')
+	{
+	n++;
+	}
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+
+	_putchar('\n');
+}
+
