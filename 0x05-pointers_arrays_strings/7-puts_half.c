112972f9885ba8629d2dc4e2ebb1d7142edbe13f
Kevin Warui <waruimacharia@gmail.com>
2023-03-27 06:30:44 -0700

    Add 7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *puts_half - print second half of string
+ *@str: string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int i, j, half;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	if (i % 2 == 0)
+		half = i / 2;
+	else
+		half = (i - 1) / 2;
+	for (j = half; j < i; j++)
+		_putchar(str[j]);
+	_putchar('\n');
+}
+
