cc5121c70e6f3a94386a2fb3e7611c9c8de7eea9
Kevin Warui <waruimacharia@gmail.com>
2023-03-27 11:30:06 -0700

    Add 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,29 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h>
-
-#define PASSWORD_LENGTH 8
 
 /**
- *main - genrate valid passwords for 101-crackme
+ * main - Generates randomvalid password for 101-crackme.
+ * Return: Always 0.
  */
 
-int main() {
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    const int charset_size = sizeof(charset) - 1;
-    char password[PASSWORD_LENGTH + 1];
-    int i;
+int main(void)
+{
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-    srand(time(NULL) ^ getpid());
+	srand(time(0));
 
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = charset[rand() % charset_size];
-    }
-    password[PASSWORD_LENGTH] = '\0';
+	while (sum < 2772)
+	{
+	password[index] = 33 + rand() % 94;
+	sum += password[index++];
+	}
 
-    printf("%s", password);
+	password[index] = '\0';
 
-    return 0;
+	if (sum != 2772)
+	{
+	diff_half1 = (sum - 2772) / 2;
+	diff_half2 = (sum - 2772) / 2;
+
+	if ((sum - 2772) % 2 != 0)
+	diff_half1++;
+
+	for (index = 0; password[index]; index++)
+	{
+	if (password[index] >= (33 + diff_half1))
+	{
+	password[index] -= diff_half1;
+	break;
+	}
+	}
+	for (index = 0; password[index]; index++)
+	{
+	if (password[index] >= (33 + diff_half2))
+	{
+	password[index] -= diff_half2;
+	break;
+	}
+	}
+	}
+	printf("%s", password);
+	return (0);
 }
-
