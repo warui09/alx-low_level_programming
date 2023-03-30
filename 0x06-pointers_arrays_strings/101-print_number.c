f3c37031724a3175ed112f131ad3e74c56463a68
Kevin Warui <waruimacharia@gmail.com>
2023-03-30 08:01:24 -0700

    Refactor 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,19 +9,15 @@ void print_number(int n)
 {
 	unsigned int n1;
 
+	n1 = n;
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
+		n1 = -n;
 	}
-	else
-	{
-		n1 = n;
-	}
-	if (n1 / 10)
+	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
-
-	_purchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
