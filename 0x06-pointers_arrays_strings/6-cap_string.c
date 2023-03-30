1511c0065e6ca3ec2ad2347eb3787abd53962e14
Kevin Warui <waruimacharia@gmail.com>
2023-03-30 03:05:31 -0700

    Fix line that was 80+ characters

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,24 @@
 #include "main.h"
 
 /**
- *cap_string - capitalize every word in a string
- *@str: string to cpitalize
- *Return: char
+ * cap_string - capitalize every word in a string
+ * @str: string to capitalize
+ * Return: char
  */
 
 char *cap_string(char *str)
 {
 	char *p = str;
-	int capitalize;
 
-	capitalize = 1;
+	int capitalize = 1;
+
+	char pn[] = {' ', '\t', '\n', ',', ';',
+		'.', '!', '?', '"', '(', ')',
+		'{', '}'};
+
+
+	int num_pn = sizeof(pn) / sizeof(pn[0]);
+
 	while (*p)
 	{
 		if (capitalize && (*p >= 'a' && *p <= 'z'))
@@ -19,18 +26,24 @@ char *cap_string(char *str)
 			*p -= 32;
 			capitalize = 0;
 		}
-		else if (*p == ' ' || *p == '\t' || *p == '\n' || *p == ',' || \
-				*p == ';' || *p == '.' || *p == '!' || \
-				*p == '?' || *p == '"' || *p == '(' || \
-				*p == ')' || *p == '{' || *p == '}')
-		{
-			capitalize = 1;
-		}
 		else
+		{
+			int i;
+
+			for (i = 0; i < num_pn; i++)
+			{
+			if (*p == pn[i])
+			{
+			capitalize = 1;
+			break;
+			}
+		}
+		if (i == num_pn)
 		{
 			capitalize = 0;
 		}
-		p++;
+	}
+	p++;
 	}
 	return (str);
 }
