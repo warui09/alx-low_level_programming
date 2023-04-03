85d483e1c98047bd6f1ddf25ffddddbdf9dbec71
Kevin Warui <waruimacharia@gmail.com>
2023-04-03 07:01:19 -0700

    Change logic to break loop iteration when no match is found

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,15 +7,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	unsigned int i, j, k;
+	int is_match;
 
-	j = 0;
+	k = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] != accept[0])
-			j++;
-		if (s[i] == accept[0])
+		is_match = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				is_match = 1;
+				break;
+			}
+		}
+		if (!is_match)
 			break;
+		else
+			k++;
 	}
-	return (j + 1);
+	return (k);
 }
