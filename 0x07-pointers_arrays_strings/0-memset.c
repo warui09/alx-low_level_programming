8b7c7c13c9a112f913fb6d5d9bcc197e4076f781
Kevin Warui <waruimacharia@gmail.com>
2023-04-03 02:57:59 -0700

    Add 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_memset - fill the first n bytes of memory with a constant byte
+ *@s: beggining of the buffer to start writing the constant byte
+ *@b: constant byte
+ *@n: number of bytes to write t
+ *Return: beginning of buffer the function has written to
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
