e73f9a8ad0873f4f22e5410c43a1e527e30e25e3
Kevin Warui <waruimacharia@gmail.com>
2023-04-05 14:01:58 -0700

    Add 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *is_prime_number_helper - this is a helper function that
+ *actually checks if the number is prime
+ *
+ *@n: number to check
+ *@divisor: number to divide n by and check for remeinder
+ *
+ *Return: int
+ */
+
+int is_prime_helper(int n, int divisor)
+{
+	if (divisor == 1)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (is_prime_helper(n, divisor - 1));
+}
+
+/**
+ *is_prime_number - check if number is prime by calling helper function
+ *
+ *@n: number to check
+ *
+ *Return: int
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_prime_helper(n, n - 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -14,3 +14,9 @@ Write a function that returns the factorial of a given number.
 
 4. Once an idea has taken hold of the brain it's almost impossible to eradicate
 Write a function that returns the value of x raised to the power of y.
+
+5. Your subconscious is looking for the dreamer
+Write a function that returns the natural square root of a number.
+
+6. Inception. Is it possible?
+Write a function that returns 1 if the input integer is a prime number, otherwise return 0.
