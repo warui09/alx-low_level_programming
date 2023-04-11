c7fd73c523d6bda55f5df5c625e7eb21484e8d35
Kevin Warui <waruimacharia@gmail.com>
2023-04-10 23:56:47 -0700

    Handle return of NULL from malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,6 +22,8 @@ char *create_array(unsigned int size, char c)
 	else
 	{
 		a = (char *) malloc(size * sizeof(char));
+		if (a == NULL)
+			return (NULL);
 		for (i = 0; i < size; i++)
 		{
 			a[i] = c;
