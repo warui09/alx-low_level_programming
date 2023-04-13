0701dd82d59874551f61fbbe571223ac4ce8ac27
Kevin Warui <waruimacharia@gmail.com>
2023-04-13 10:30:11 -0700

    Change stop condition in loop

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,8 +4,8 @@
 /**
  * _calloc - create an array and initialize it to 0
  *
- * @nmemb: number or sub-arrays
- * @size: size of each sub-array
+ * @nmemb: number of elemts in array
+ * @size: size of each element
  *
  *Return: char
  */
@@ -19,10 +19,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	a = malloc(nmemb * size);
-	if (!a)
+	if (a == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb * size; i++)
+	for (i = 0; i < nmemb; i++)
 		a[i] = 0;
 
 	return (a);
