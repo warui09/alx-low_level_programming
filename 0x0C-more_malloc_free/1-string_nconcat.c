49dcc0237a6cc6c1c40824e3d2a1525168f97edc
Kevin Warui <waruimacharia@gmail.com>
2023-04-13 03:18:11 -0700

    Refactor the entire function because of code fail

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,30 +3,38 @@
 #include <string.h>
 
 /**
- *string_nconcat - concatenates n bytes of s2 to s1
+ * string_nconcat - concatenates n bytes of s2 to s1
  *
- *@s1: string one
- *@s2: string two
- *@n: number of bytes of s2 to concatenate to s1
+ * @s1: string one
+ * @s2: string two
+ * @n: number of bytes of s2 to concatenate to s1
  *
- *Return: char
+ * Return: char
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, k, l;
+	unsigned int i, j, len1, len2;
 	char *a;
 
-	i = strlen(s1);
-	l = strlen(s2);
-	a = malloc(sizeof(s1) + sizeof(char) * n + 1);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	if (n >= len2)
+		a = malloc(sizeof(char) * (len1 + len2 + 1));
+	else
+		a = malloc(sizeof(char) * (len1 + n + 1));
 	if (a == NULL)
 		return (NULL);
-	if (n >= l)
-		n = l;
-	for (k = 0; k < i; k++)
-		a[k] = s1[k];
-	for (j = 0; j < n; j++)
-		a[j + k] = s2[j];
+
+	for (i = 0; i < len1; i++)
+		a[i] = s1[i];
+	for (j = 0; j < n && j < len2; j++)
+		a[i + j] = s2[j];
+	a[i + j] = '\0';
+
 	return (a);
 }
