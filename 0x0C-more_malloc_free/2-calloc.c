9affedf04acead7725ec708ae070f7b238792235
Kevin Warui <waruimacharia@gmail.com>
2023-04-13 12:40:04 -0700

    Remove loop

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdlib.h>
+#include <string.h>
 
 /**
  * _calloc - create an array and initialize it to 0
@@ -14,7 +14,6 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *a;
-	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
