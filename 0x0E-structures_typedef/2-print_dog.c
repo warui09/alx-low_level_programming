0159303e0a7be5a9badab4dc3ca9f27d5382541c
Kevin Warui <waruimacharia@gmail.com>
2023-04-17 09:15:33 -0700

    Refactored code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,11 +14,19 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+
 	if (d->name == NULL)
 		printf("(nil)\n");
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
+	else
+		printf("Name: %s\n", d->name);
+
+	if (!d->age)
+		printf("(nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
 	if (d->owner == NULL)
 		printf("(nil)\n");
-	printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: %s\n", d->owner);
 }
