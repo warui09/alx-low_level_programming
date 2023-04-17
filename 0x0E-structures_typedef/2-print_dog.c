afceacd9e367a1f6797779e9428d49313f283731
Kevin Warui <waruimacharia@gmail.com>
2023-04-17 11:38:33 -0700

    Change conditional print logic

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,10 +16,14 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		printf("(nil)\n");
-	printf("Name: %s\n", d->name);
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
 	printf("Age: %f\n", d->age);
+
 	if (d->owner == NULL)
-		printf("(nil)\n");
-	printf("Owner: %s\n", d->owner);
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
