02b770399baae5745894a16bcd00c10d360b98da
Kevin Warui <waruimacharia@gmail.com>
2023-04-17 12:26:59 -0700

    Add 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,29 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct *d;
+	dog_t *a;
+	int i;
 
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
+	a = malloc(sizeof(dog_t));
+	if (a == NULL)
 		return (NULL);
 
-	
+	a->name = malloc(sizeof(name));
+	a->owner = malloc(sizeof(owner));
+	if (a->name == NULL || a->owner == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+
+	for (i = 0; name[i] != '\0'; i++)
+		a->name[i] = name[i];
+	a->name[i] = '\0';
+
+	for (i = 0; owner[i] != '\0'; i++)
+		a->owner[i] = owner[i];
+	a->owner[i] = '\0';
+
+	a->age = age;
+	return (a);
+}
