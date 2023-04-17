8e7ef1c4998a81ad4dcbbda556ec0001f1b4cb0f
Kevin Warui <waruimacharia@gmail.com>
2023-04-17 08:58:58 -0700

    Remove logic to check id age is null

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,8 +17,6 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		printf("nil\n");
 	printf("Name: %s\n", d->name);
-	if (!d->age)
-		printf("nil\n");
 	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("nil\n");
