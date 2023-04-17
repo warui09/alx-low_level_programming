67b3a52a856410909ecb794b32ebcb5642606ff0
Kevin Warui <waruimacharia@gmail.com>
2023-04-17 09:01:49 -0700

    Edit string to print if name or owner is NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,10 +15,10 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("nil\n");
+		printf("(nil)\n");
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("nil\n");
+		printf("(nil)\n");
 	printf("Owner: %s\n", d->owner);
 }
