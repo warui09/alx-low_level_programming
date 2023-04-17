b8a2984d60fb728956ba43950f4357affc91aa45
Kevin Warui <waruimacharia@gmail.com>
2023-04-17 05:38:10 -0700

    Add documentation for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog - holds information about a dog
+ *@name: name of dog
+ *@age: age of dog in years
+ *@owner: name of owner of dog
+ */
+
 struct dog
 {
 	char *name;
