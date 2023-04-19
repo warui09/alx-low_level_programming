068c30743160a806dcf786b10c93f2d741f28e57
Kevin Warui <waruimacharia@gmail.com>
2023-04-19 05:49:09 -0700

    Edit documentation and check for NULL arguments

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,18 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  *print_name - prints a name
  *
  *@name: name to print
- *@(*f): pointer to callback function
+ *@f: pointer to callback function
  *
  *Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
