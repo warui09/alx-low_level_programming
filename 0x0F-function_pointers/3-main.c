0f2dbda22faa1f87045d9125c65415e37875f981
Kevin Warui <waruimacharia@gmail.com>
2023-04-21 12:50:55 -0700

    Add 3-calc.h 3-get_op_func.c 3-main.c and 3-op_functions.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *main - entry point
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	char *s;
+	int a, b, c;
+
+	s = argv[2];
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (*s != '+' && *s != '-' && *s != '*' && *s != '/' && *s != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*s == '/' && b == 0) || (*s == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	c = (*get_op_func(s))(a, b);
+	printf("%d\n", c);
+	return (0);
+}
