f7705b09bfa4d4736b3fb37716a81417e844b711
Kevin Warui <waruimacharia@gmail.com>
2023-04-27 04:50:23 -0700

    Add 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *list_len - returns the number of elemnts in list h
+ *@h: linked list
+ *
+ *Return: Always size_t
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	for (; h->next; h = h->next)
+		i++;
+
+	return (i + 1);
+}
