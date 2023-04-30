9c073dd5bd195413c13f58cf737ad509a1de00e9
Kevin Warui <waruimacharia@gmail.com>
2023-04-30 02:25:05 -0700

    Add 2-gitignore.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *add_nodeint - adds a new node at the beginning of a listint_t list
+ *@head: pointer to the beginning of the list
+ *@n: int value of the node to add to the list
+ *
+ *Return: Address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		*head = new_node;
+	}
+
+	return (*head);
+}
