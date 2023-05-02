6ba1841eb75afc46b4924b08858765a95ca3762c
Kevin Warui <waruimacharia@gmail.com>
2023-05-02 06:20:59 -0700

    Remove trailing white space and check if head is NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *temp;
 	unsigned int i;
 
+	if (!head)
+		return (NULL);
+
 	i = 0;
 	temp = *head;
 	new_node = malloc(sizeof(listint_t));
@@ -25,6 +28,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
 	while (temp)
 	{
 		if (i + 1 == idx)
@@ -33,10 +43,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp->next = new_node;
 			return (new_node);
 		}
-		
+
 		i++;
 		temp = temp->next;
 	}
 
+	free(new_node);
 	return (NULL);
 }
