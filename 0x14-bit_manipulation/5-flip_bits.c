a6676550998f1421748e41ce32d49a9586d6d62a
Kevin Warui <waruimacharia@gmail.com>
2023-05-05 13:14:26 -0700

    Fix line over 80 characters

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- *flip_bits - returns the number of bits needed to flip to get from one number to another
+ *flip_bits - returns the number of bits needed to flip to get
+ *            from one number to another
  *@n: first number
  *@m: second number
  *
