0d50acec4de7908d7f53d2ca98e064ed24944b24
Kevin Warui <waruimacharia@gmail.com>
2023-05-05 13:32:02 -0700

    Add 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *get_endianness - get endianness
+ *
+ *Return: int
+ */
+
+int get_endianness(void)
+{
+	int i;
+	char *a;
+
+	i = 0x76543210;
+	a = (char*) &i;
+
+	if (*a == 0x10)
+		return (1);
+	return (0);
+}
