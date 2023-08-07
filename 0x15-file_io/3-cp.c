0151a36e49fbbd36f58351e7a2a9a48a64414243
Kevin Warui <waruimacharia@gmail.com>
2023-08-06 21:18:52 -0700

    Add 1-create_file.c 2-append_text_to_file.c 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 1 on success and -1 on failure
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	int file_from_desc, file_to_desc;
+	ssize_t num_read, num_written;
+	char buffer[1024];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
+		exit(97);
+	}
+
+	file_from_desc = open(argv[1], O_RDONLY);
+	if (file_from_desc < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_to_desc = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (file_to_desc < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((num_read = read(file_from_desc, buffer, 1024)) > 0)
+	{
+		num_written = write(file_to_desc, buffer, num_read);
+		if (num_written != num_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (num_read < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (close(file_from_desc) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[1]);
+		exit(100);
+	}
+
+	if (close(file_to_desc) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %s\n", argv[2]);
+		exit(100);
+	}
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 /* prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
