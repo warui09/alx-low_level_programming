1f8e6e3c533256e8239436016b7469cd93174a35
Kevin Warui <waruimacharia@gmail.com>
2023-09-08 04:18:22 -0700

    Refactor 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,24 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
 		return (NULL);
 
 	ht->size = size;
+
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
 	return (ht);
 }
