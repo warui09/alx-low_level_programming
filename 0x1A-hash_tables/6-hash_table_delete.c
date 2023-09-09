805219fd81c742999214ed93826b923546fbe77c
Kevin Warui <waruimacharia@gmail.com>
2023-09-09 03:53:19 -0700

    Refactor 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,11 +10,12 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *node, *temp;
+	unsigned long int i;
 
 	if (ht == NULL)
 		return;
 
-	for (unsigned long int i = 0; i < ht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
 		while (node)
